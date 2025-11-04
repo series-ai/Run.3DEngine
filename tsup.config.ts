633cbde12f8f9f281a45dd59b337c679f005c7ee
Zee Vasilyev <zeejfps@gmail.com>
2025-11-04 13:10:47 -0600

    Fixed issue with venus-three not building properly

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/systems/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,          // optional: clears the output folder before build
+  sourcemap: true,      // optional: generates source maps
+  tsconfig: 'tsconfig.json',
+});
