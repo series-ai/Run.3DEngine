2663c25fd7bc8769a0a6b8519ab500416ac5722d
Zee <zvasilyev@series.ai>
2026-02-02 08:58:06 -0600

    Updated to Rundot Game SDK (#4)
    * Updated to use the latest venus SDK
    
    * Fixed RundotGameAPI export
    
    ---------
    
    Co-authored-by: Zee Vasilyev <zeejfps@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 ﻿export * from "./engine"
-export * from "@series-inc/venus-sdk"
-export { default as VenusAPI } from "@series-inc/venus-sdk/api"
+export * from "@series-inc/rundot-game-sdk"
+export { default as RundotGameAPI } from "@series-inc/rundot-game-sdk/api"
